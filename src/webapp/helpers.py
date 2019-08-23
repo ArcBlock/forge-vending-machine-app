0bae47bac6ed44e4226f3d08cda72f0a6105e1bb
Yongxin Xu <yongxin@arcblock.io>
2019-08-23 14:19:15 -0700

    clean files and add comments

--- src/webapp/helpers.py
+++ src/webapp/helpers.py
@@ -5,51 +5,60 @@ sys.path.append('./src')
 from utils.chain import get_account_states
 from utils.wallets import vending_machines, operators, manufacturers, suppliers, locations
 
-# get account state
 
-def get_parties_addresses():
+# get a list of dicts of parties' wallet info (address, pk, sk, and moniker)
+PARTY_LIST = [operators, manufacturers, suppliers, locations]
+
+def get_parties_addresses(lst: list):
+    '''
+    Grab the addresses of all parties,
+    a helper function for get_parties_states
+    ------
+    Args:
+        lst(list): list of dicts of parties's wallet info (address, pk, sk, and moniker)
+
+    Output:
+        addresses(list): list of parties' addresses
+    '''
+
     addresses = []
-    for party in [operators, manufacturers, suppliers, locations]:
+    for party in lst:
         addresses = [*addresses, *[item['address'] for item in party]]
     return addresses
 
 
-def get_parties_states():
-    return get_account_states(get_parties_addresses())
+def get_parties_states(lst: list):
+    '''
+    Get the states of every party's wallets
+    ------
+    Args:
+        lst(list): list of dicts of parties's wallet info (address, pk, sk, and moniker)
+
+    Output:
+        (list): a list of states of the given addresses, 
+            where each state is a dict with a party's wallet info (balance, num_txs, address, and moniker)
+    '''
+
+    return get_account_states(get_parties_addresses(lst))
 
 
-# def check_type(moniker: str):
-#     '''
-#     Check the type of a party
-#     '''
-#     for t in ['operator', 'manufacturer', 'supplier', 'location']:
-#         if t in moniker:
-#             return t
-#         continue
-#     raise ValueError("Oops, no match type is found")
-# print(check_type("hh001"))
+def party_types(lst: list):
+    '''
+    Get the type of each party, where type includes operator, manufacturer, supplier, and location,
+    a helper function for building RadioItems and Dropdown.
+    ------
+    Args:
+        lst(list): list of dicts of parties's wallet info (address, pk, sk, and moniker)
 
-
-# get control dict
-def party_types():
+    Output:
+        type(dict): a dict where key and value are wallet monikers
+    '''
+    
     types = {}
-    for party in get_parties_states():
+    for party in get_parties_states(lst):
         moniker = party['moniker']
         types.update({'{}'.format(moniker): moniker})
     return types
 
 
-PARTY_TYPES = party_types()
-
-# # add user readable info to aggregate tx list
-# def readable_aggregate_tx_lst():
-#     lst = list_aggregate_tx()
-#     for d in lst:
-#         d.update(dict(
-#             vm = get_account_moniker(d['vm_addr']),
-#             operator = get_account_moniker(d['op_addr']),
-#             manufacturer = get_account_moniker(d['ma_addr']),
-#             supplier = get_account_moniker(d['su_addr']),
-#             location = get_account_moniker(d['lo_addr']),
-#         ))
-#     return lst
+PARTY_TYPES = party_types(PARTY_LIST)
